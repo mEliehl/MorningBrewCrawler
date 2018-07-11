c63df93f8572f5a4a9cf8e223e88166563d37fc1
marcos.santos <marcos.santos@dextra-sw.com>
2018-07-10 23:09:15 -0300

    added generic host

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using System.IO;
 using System.Threading.Tasks;
-using Crawler.Commands;
-using Crawler.HttpFactories;
-using Crawler.Repositories;
-using Crawler.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Runner
 {
@@ -11,16 +11,47 @@ namespace Runner
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Starting...");
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", optional: true);
+                    configApp.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                    configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .UseConsoleLifetime()
+                .Build();
 
-            IHttpHandler httpHandler = new MorningbrewHttpHandler();
-            IArticles articles = new Articles();
-            var commandHandler = new CrawlerCommandHandler(httpHandler,articles);
-            var command = new CrawlerCommand(1,50);
-            await commandHandler.HandlerAsync(command);
+            await host.RunAsync();
+
+            // Console.WriteLine("Starting...");
+
+            // IHttpHandler httpHandler = new MorningbrewHttpHandler();
+            // IArticles articles = new Articles();
+            // var commandHandler = new CrawlerCommandHandler(httpHandler,articles);
+            // var command = new CrawlerCommand(1,50);
+            // await commandHandler.HandlerAsync(command);
 
 
-            System.Console.WriteLine("Finished");
+            // System.Console.WriteLine("Finished");
         }
     }
 }
