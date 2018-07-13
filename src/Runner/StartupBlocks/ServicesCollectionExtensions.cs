fefe09f023899e74d6b95e028ba4d5fd5c44efd4
marcos.santos <marcos.santos@dextra-sw.com>
2018-07-13 10:28:57 -0300

    added polly

--- src/Runner/StartupBlocks/ServicesCollectionExtensions.cs
+++ src/Runner/StartupBlocks/ServicesCollectionExtensions.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Net.Http;
 using Crawler.Commands;
 using Crawler.HttpClients;
 using Crawler.Repositories;
 using Crawler.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Timeout;
 
 namespace Runner.StartupBlocks
 {
@@ -12,14 +16,28 @@ namespace Runner.StartupBlocks
     {
         public static IServiceCollection AddHttpClients(this IServiceCollection services)
         {
+            var retryPolicy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .Or<TimeoutRejectedException>()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                });
+
+            var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(10);
+
+
             services
-            .AddTransient<TimingHandler>()
             .AddHttpClient<IMorningBrewClient, MorningBrewClient>(client =>
             {
                 client.BaseAddress = new Uri("http://blog.cwa.me.uk/");
 
-            }).AddHttpMessageHandler<TimingHandler>();
-            
+            })
+            .AddPolicyHandler(retryPolicy)
+            .AddPolicyHandler(timeoutPolicy); // We place the timeoutPolicy inside the retryPolicy, to make it time out each try.
+
             return services;
         }
 
