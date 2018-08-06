80d2a6186e7412863ebda0270763280dc672bc0b
marcos.santos <marcos.santos@dextra-sw.com>
2018-08-06 14:07:34 -0300

    added fluent migrator

---
+++ src/SqlServer.Migration.Fluent/Migrations/CreateArticleTable.cs
@@ -0,0 +1,24 @@
+
+using FluentMigrator;
+
+namespace SqlServer.Migration.Fluent.Migrations
+{
+    [Migration(20180806131500)]
+    public class CreateArticleTable : FluentMigrator.Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Article")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Date").AsDateTime()
+                .WithColumn("Link").AsString()
+                .WithColumn("Title").AsString()
+                .WithColumn("Authors").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Article");
+        }
+    }
+}
---
+++ src/SqlServer.Migration.Fluent/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using DbUp;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SqlServer.Migration.Fluent.Migrations;
+
+namespace SqlServer.Migration.Fluent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var serviceProvider = CreateServices(configuration);
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                EnsureDatabase.For.SqlDatabase(configuration.GetConnectionString("default"));
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices(IConfigurationRoot configuration)
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(configuration.GetConnectionString("default"))
+                    .ScanIn(typeof(CreateArticleTable).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+    }
+}
