02146eac5d1464449cd951f95109760c15903230
marcos.santos <marcos.santos@dextra-sw.com>
2018-08-15 20:57:55 -0300

    Add migration script to DbUp
    Add create author's table and data migration sql scripts

---
+++ src/SqlServer.Migration.DbUp/Scripts/Script0002-CreateAuthorTable.sql
@@ -0,0 +1,26 @@
+use MorningBrew;
+
+Create Table Author(
+    ArticleId uniqueidentifier,
+    Name NVARCHAR(max),
+    FOREIGN KEY (ArticleId) REFERENCES Article(Id) ON DELETE CASCADE
+);
+
+CREATE TABLE #CommaDelimiter
+(
+	[Id] [uniqueidentifier] NOT NULL,	
+	[Authors] [nvarchar](max) NOT NULL
+ 
+)
+
+Insert Into #CommaDelimiter
+select Id ,TRIM(value) as Authors
+from Article
+CROSS APPLY string_split(Authors,',')
+
+insert into Author
+select Id as ArticleId, TRIM(value) as Name
+from #CommaDelimiter
+CROSS APPLY string_split(Authors,'&');
+
+ALTER TABLE Article DROP COLUMN Authors;
