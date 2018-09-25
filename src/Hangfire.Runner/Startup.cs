009b3764e5024a543487fada6b8e6ace4655c38c
marcos.santos <marcos.santos@dextra-sw.com>
2018-09-25 20:16:23 -0300

    Add Hangfire server
    In the next implementation, we will change generic host
    to hangfire

---
+++ src/Hangfire.Runner/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbUp;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Hangfire.Runner
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var cs = this.Configuration.GetConnectionString("hangfire");
+            EnsureDatabase.For.SqlDatabase(cs);
+            services.AddHangfire(configuration =>
+            {
+                configuration.UseSqlServerStorage(cs);
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+            app.UseMvc();
+        }
+    }
+}
