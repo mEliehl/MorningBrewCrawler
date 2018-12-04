236148558acf731c993ded1b65711e4936c16240
marcos.santos <marcos.santos@dextra-sw.com>
2018-12-04 17:11:21 -0200

    Update to Dotnet 2.2

--- src/Hangfire.Runner/Startup.cs
+++ src/Hangfire.Runner/Startup.cs
@@ -29,7 +29,7 @@ namespace Hangfire.Runner
                .AddRepositories(Configuration)
                .AddCommanHandlers();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
